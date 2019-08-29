29ac023c46ac7341fdb2354ffa4320e82e6103cf
Ashish Padakannaya <ashish.mlr.123@gmail.com>
2019-08-28 19:15:17 -0700

    adding insert, prune and plot code

--- new-rect.py
+++ new-rect.py
@@ -68,6 +68,82 @@ class MaxRectsBinPack:
 					bestlongsidefit=flippedLongSideFit
 		return bestnode,bestshortsidefit,bestlongsidefit
 
+	def Insert(self,width,height):
+		score1=0
+		score2=0
+		newNode,score1,score2=self.FindPositionFornewNode(width,height,score1,score2)
+
+		#if rectangle packing is unsuccessful
+		if newNode.height==0:		
+			return newNode
+		size=len(self.freeRectangles)
+		i=0
+		while i < size:
+			if self.SplitfreeNode(self.freeRectangles[i],newNode):	#the splitting
+				del self.freeRectangles[i]							#deletes the rectangles after it's been split up
+				i=i-1
+				size=size-1
+			i=i+1
+
+		#checks for redundant entries
+		self.prunefreelist()										
+		self.usedRectangles.append(newNode)
+		return newNode
+
+	def prunefreelist(self):
+		i=0
+		while (i<len(self.freeRectangles)):
+			j=i+1
+			while(j<len(self.freeRectangles)):
+				if self.IsContainedIn(self.freeRectangles[i],self.freeRectangles[j]):
+					del self.freeRectangles[i]
+					i=i-1
+					break
+				if self.IsContainedIn(self.freeRectangles[j],self.freeRectangles[i]):
+					del self.freeRectangles[j]
+					j=j-1
+				j=j+1
+			i=i+1
+	
+	def IsContainedIn(self,a,b):
+		#evaluates co-ordinates and measures of both the rectangles
+		k1=a.x >= b.x and a.y >= b.y
+		k2=a.x + a.width <= b.x + b.width
+		k3=a.y + a.height <= b.y + b.height
+		return k1 and k2 and k3			
+	
+	def SplitfreeNode(self,freeNode,usedNode):
+		#checking if the rectangles intersect
+		if usedNode.x>=freeNode.x+freeNode.width or usedNode.x+usedNode.width<=freeNode.x or usedNode.y>=freeNode.y+freeNode.height or usedNode.y+usedNode.height <= freeNode.y:
+			return False
+		
+		#splitting begins
+		if	usedNode.x<freeNode.x+freeNode.width and usedNode.x+usedNode.width>freeNode.x:
+			if usedNode.y > freeNode.y and usedNode.y<freeNode.y+freeNode.height:
+				#if usedNode is at the top side
+				newNode=Rect(freeNode)
+				newNode.height=usedNode.y-newNode.y
+				self.freeRectangles.append(newNode)
+			if usedNode.y+usedNode.height<freeNode.y+freeNode.height:
+				#if usedNode is at the top side
+				newNode=Rect(freeNode)
+				newNode.y=usedNode.y+usedNode.height
+				newNode.height=freeNode.y + freeNode.height- (usedNode.y+usedNode.height)
+				self.freeRectangles.append(newNode)
+		if usedNode.y<freeNode.y+freeNode.height and usedNode.y+usedNode.height>freeNode.y:
+			if usedNode.x > freeNode.x and usedNode.x<freeNode.x+freeNode.height:
+				#if usedNode is on the left
+				newNode=Rect(freeNode)
+				newNode.width=usedNode.x-newNode.x
+				self.freeRectangles.append(newNode)
+			if usedNode.x + usedNode.width < freeNode.x + freeNode.width:
+				#if usedNode on the right
+				newNode=Rect(freeNode)
+				newNode.x=usedNode.x+usedNode.width
+				newNode.width=freeNode.x+freeNode.width-(usedNode.x+usedNode.width)
+				self.freeRectangles.append(newNode)
+		return True
+
 
 if __name__ == "__main__":
 
@@ -92,3 +168,32 @@ if __name__ == "__main__":
 	height=items[2][0]
 	del items[0:3]
 	maxbin=MaxRectsBinPack(int(width),int(height))
+
+	for i in range (0,n):
+		print "**********************************"
+		print "packing bin of size",items[i][1],"   ",items[i][2]
+		packedrect=Rect()
+		packedrect=maxbin.Insert(items[i][1],items[i][2])
+		if packedrect.height>0:
+			print "bin packed successfully"
+		else :
+			print "no space"
+
+	# saving output to bokeh file
+	html_title="height=%r and width='%r'"%(height,width)
+	bk.output_file("maxrects.html",title="MAXRECTS")				
+	bk.figure(plot_width=600,plot_height=600,title=html_title)
+	bk.hold()														
+	x_cor=[]
+	y_cor=[]
+	b_width=[]
+	b_height=[]
+
+	# plotting
+	bk.rect([width/2],
+		[int(height)/2],
+		[width],
+		[int(height)],fill_color="crimson")
+
+	#disables overwriting of any previous glyph
+	bk.hold()
